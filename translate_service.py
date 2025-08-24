64a23fc63b0154a5eb12d9d83a77e5dcf42986e3
hyungyu-lee <gusrb0808@naver.com>
2025-08-24 21:55:14 +0900

    간단 번역 서비스 예시

---
+++ translate_service.py
@@ -0,0 +1,32 @@
+import os
+import streamlit as st
+from langchain_community.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+
+os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
+
+langs = ["Korean", "Japanese", "Chinese", "English"]
+left_co, cent_co, last_co = st.columns(3)
+
+with st.sidebar:
+    language = st.radio('번역을 원하는 언어를 선택해주세요.:', langs)
+
+st.markdown('### 언어 번역 서비스입니다^^')
+prompt = st.text_input('번역을 원하는 텍스트 입력하시오')
+
+trans_template = PromptTemplate(
+    input_variables = ['trans'],
+    template = 'Your task is to translate this text to ' + language + ' TEXT: {trans}'
+)
+
+memory = ConversationBufferMemory(input_key = 'trans', memory_key = 'chat_history')
+
+llm = ChatOpenAI(temperature = 0.0, model_name = 'gpt-4')
+trans_chain = LLMChain(llm = llm, prompt = trans_template, verbose = True, output_key = 'translate', memory = memory)
+
+if st.button('번역'):
+    if prompt:
+        response = trans_chain({'trans': prompt})
+        st.info(response['translate'])
